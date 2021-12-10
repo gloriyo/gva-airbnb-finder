36c5dccbf97ec1360703a10e9019819ffed73ad6
gloriyo <gloriajyoon@gmail.com>
2021-12-09 22:09:36 -0800

    accept neighbourhood input

--- code/find_hotel.py
+++ code/find_hotel.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from tabulate import tabulate
 from difflib import get_close_matches
 
 '''
@@ -29,6 +28,8 @@ osm_data = pd.read_json('clean-amenities.json')
 airbnb_data = pd.read_csv('listings.csv')
 neighbourhood_data = pd.read_csv('clean-neighbourhood.csv')
 
+neighbourhood_options = neighbourhood_data['neighbourhood'].to_numpy()
+
 # Get user's picks
 def input_prio(remaining_options):
     pick = input('Please pick your highest priority from the following options:\n - {}\n\n'.format('\n - '.join(remaining_options)))
@@ -50,8 +51,20 @@ def input_prio(remaining_options):
     
     return pick, remaining_options
 
-def input_neighbourhood(neighbourhood):
-    return
+def input_neighbourhood():
+    pick = input('Please pick your highest priority from the following options:\n - {}\n\n'.format('\n - '.join(neighbourhood_options)))
+    try:
+        pick = get_close_matches(pick, neighbourhood_options, n=1, cutoff=0.6)[0] # Typo correction
+    except IndexError:
+        pick = None
+
+    while pick not in neighbourhood_options:
+        pick = input('\nThat was not an option. Please pick your highest priority from the following options:\n - {}\n\n'.format('\n - '.join(neighbourhood_options)))
+        try:
+            pick = get_close_matches(pick, neighbourhood_options, n=1, cutoff=0.6)[0] # Typo correction
+        except IndexError:
+            pick = None
+    return pick
 
 # User must sort in what their priority is
 options = ['food', 'transportation', 'parking', 'tourist attraction']
@@ -61,7 +74,8 @@ prio2, options = input_prio(options)
 prio3, options = input_prio(options)
 prio4 = options[0]
 
-neighbourhood = input_neighbourhood(neighbourhood_options)
+neighbourhood = input_neighbourhood()
+# print(neighbourhood)
 
 priorities = prio1, prio2, prio3, prio4
 
