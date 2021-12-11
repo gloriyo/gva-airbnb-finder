7f7c239444c14e061743105c72828db09d570a42
J-Santella <jarell.santella@gmail.com>
2021-12-10 18:41:16 -0800

    refactor

--- code/map_visual.py
+++ code/map_visual.py
@@ -98,6 +98,6 @@ if __name__=='__main__':
     in_directory = sys.argv[1]
     out_directory = sys.argv[2]
     
-    top_airbnb_data = 'top_airbnb_data.csv'
+    top_airbnb_data = 'top-airbnb-data.csv'
     clean_amenities_data = 'clean-amenities.json'
     main(top_airbnb_data, clean_amenities_data,)
