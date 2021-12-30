9cc2673bd35d90bb545b0c24a7de9e2c321fe89a
gya13 <gloria_yoon@sfu.ca>
2021-12-29 22:49:23 -0800

    set up basic app with navbar

---
+++ airbnb-app/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+class NavBar extends Component {
+    render() { 
+        return (
+            <Fragment>
+                <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="/">Welcome</Navbar.Brand>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/Home">Home</Nav.Link>
+                            <Nav.Link href="#">Feature 1</Nav.Link>
+                            <Nav.Link href="#">Feature 2</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
+            </Fragment>
+        );
+    }
+}
+ 
+export default NavBar;
