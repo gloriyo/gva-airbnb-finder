690f981b5ee2e19242c1ff99c4d1d69775b988c6
gya13 <gloria_yoon@sfu.ca>
2022-01-09 01:01:02 -0800

    minor comment clean up

--- server/index.js
+++ server/index.js
@@ -29,7 +29,6 @@ var http = require('http');
 // app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use(express.static(path.join(__dirname, ".." ,'build')));
 
-// console.log("aaaaaaaaa")
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -46,9 +45,6 @@ app.get('/api/list', (req, res) => {
 app.get("/api", (req, res) => {
 	res.json({ message: "Hello from server!" });
 });
-
-
-
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,4 +1,6 @@
 
+// routes currently not in use
+
 module.exports=function(app){
     app.get('/',function(req,res){
        res.render('index');
